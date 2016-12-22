50fa8409eb702f93eac94895ff808df039dc8deb
Simon Schwartz <simon.schwartz0@gmail.com>
2016-12-22 15:35:51 +1100

    update gatekeeper url

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var config = {
   production: {
     client_id: '2ca85a89c5f1a9b7dca3',
     redirect_uri: 'http://simon.local:9966/',
-    gatekeeper: 'http://127.0.0.1:9999'
+    gatekeeper: 'https://codeflight-gatekeeper.herokuapp.com/'
   }
 }
 
